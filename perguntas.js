ebe3841e09639190b1a3dc6824dff5b7315fdd6b
Matheus-Gomes10 <matheus.gomes21@escola.pr.gov.br>
2024-10-18 12:26:29 -0300

    18/10

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Filmes',
+    'Nome do filme da franquia Velozes e Furiosos que se passa em Tóquio?',
+    'Velozes e Furiosos Desafio em Tóquio'
+)
+criaCartao(
+    'Mecânica',
+    'Peça do motor do carro que incedeia a gasolina no motor',
+    'Vela'
+)
+criaCartao(
+    'Motos',
+    'Nome comercial da moto Honda XR300L?',
+    'Tornado'
+)
+criaCartao(
+    'Carro',
+    'Qual a litragem do motor do Chevrolet Celta?',
+    '1.0'
+)
