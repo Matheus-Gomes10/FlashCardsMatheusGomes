2a9d7c7bb5ccee8eeede5df43e139c28fbbfa0eb
Matheus-Gomes10 <matheus.gomes21@escola.pr.gov.br>
2024-10-25 08:55:20 -0300

    25/10

--- aap.js
+++ aap.js
@@ -1,4 +1,19 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById(container);
     let cartao = document.createElement('article');
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+                    <h3>Filmes</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>Nome do filme da franquia Velozes e Furiosos que se passa em Tóquio?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>Velozes e Furiosos Desafio em Tóquio</p>
+                    </div>
+                </div>
+    `
+
+    container.appendChild(cartao);
 }
